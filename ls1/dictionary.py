fa044e727b5edca8eb7dc487a6222b105461e093
Sergey Semenyuk <sergey.semenyuk@gentechpartners.com>
2018-03-29 18:54:01 +0300

    update ls1

--- ls1/dictionary.py
+++ ls1/dictionary.py
@@ -13,3 +13,15 @@ d2['key3']['key123'][2]
 print(d2['key3']['key123'][2].upper())
 
 
+#>>> rec[‘name’] # ‘Name’ – это вложенный словарь
+#{‘last’: ‘Smith’, ‘first’: ‘Bob’}
+#>>> rec[‘name’][‘last’] # Обращение к элементу вложенного словаря
+#‘Smith’
+#>>> rec[‘job’] # ‘Job’ – это вложенный список
+#[‘dev’, ‘mgr’]
+#>>> rec[‘job’][-1] # Обращение к элементу вложенного списка
+#‘mgr’
+#>>> rec[‘job’].append(‘janitor’) # Расширение списка должностей Боба (Bob)
+#>>> rec
+#{‘age’: 40.5, ‘job’: [‘dev’, ‘mgr’, ‘janitor’], ‘name’: {‘last’: ‘Smith’,
+#‘first’: ‘Bob’}}
--- ls1/for.py
+++ ls1/for.py
@@ -9,3 +9,4 @@ list = [1,2,3,4,5,6]
 for i in list:
     print(i)
 
+
